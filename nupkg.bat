368ed58de84f49d602d7f98f02bd48f28539058b
shenlos <matt.fang@live.cn>
2016-11-02 22:45:41 +0800

    删除文件

--- nupkg.bat
+++
@@ -1,3 +0,0 @@
-nuget pack ../Dukou.Config/Dukou.Config.csproj -IncludeReferencedProjects -properties Configuration=Release
-nuget pack ../Dukou.Cryptography/Dukou.Cryptography.csproj -IncludeReferencedProjects -properties Configuration=Release
-nuget pack ../Dukou.SH4/Dukou.SH4.csproj -IncludeReferencedProjects -properties Configuration=Release
